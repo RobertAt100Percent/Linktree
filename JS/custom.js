ca3c1d5d177bf6579843ba06af5c3ed64e2fab8d
Robert Daniels Jr <broozerdaniels@yahoo.com>
2023-06-07 17:21:03 -0400

    updated javascript to go up when arrow is clicked

--- JS/custom.js
+++ JS/custom.js
@@ -1,28 +1,64 @@
-
-/* Template Name   : Treelink, mobile page for links.
+/* 
+* Template Name    : Treelink, the best "link in bio" responsive template.
 * Author           : Jorge Perez
-* Version          : 1.0.2
-* Updated          : Nov 2022
-* File Description : Main .js file of the template   */
-
+* Version          : 2.0.0
+* Updated          : June 2023
+* File Description : Main JavaScript file ----------------------------------- */
 
 ! function($) {
     "use strict";
 
-    var Treelink = function() {};
-
-    //PreLoader
-    Treelink.prototype.initPreLoader = function() {
-        $('#status').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
-    },
-
-    Treelink.prototype.init = function() {
-        this.initPreLoader();
-    },
+    class Treelink {
+        constructor() { }
+        //PreLoader
+        initPreLoader() {
+            $('#status').fadeOut();
+            $('#preloader').delay(350).fadeOut('slow');
+            $('body').delay(350).css({
+                'overflow': 'visible'
+            });
+        }
+        //scroll
+        initStickyMenu() {
+            var navbar = document.querySelector('nav');
+            window.onscroll = function () {
+                // pageYOffset or scrollY
+                if (window.pageYOffset > 30) {
+                    navbar.classList.add('stickyadd');
+                } else {
+                    navbar.classList.remove('stickyadd');
+                }
+            };
+        }
+        //Scrollspy
+        initScrollspy() {
+            var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+                target: '#main_nav',
+                offset: 70
+            });
+        }
+        // BACK TO TOP
+        initBackToTop() {
+            $(window).on('scroll', function () {
+                if ($(this).scrollTop() > 100) {
+                    $('.back_top').fadeIn();
+                } else {
+                    $('.back_top').fadeOut();
+                }
+            });
+            $('.back_top').click(function () {
+                $("html, body").animate({ scrollTop: 0 }, 1000);
+                return false;
+            });
+        }
+        init() {
+            this.initPreLoader();
+            this.initStickyMenu();
+            this.initScrollspy();
+            this.initBackToTop();
+        }
+        
+    }
     //init
     $.Treelink = new Treelink, $.Treelink.Constructor = Treelink
 }(window.jQuery),
